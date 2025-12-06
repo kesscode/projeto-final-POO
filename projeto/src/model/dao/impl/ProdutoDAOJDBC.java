d2580315c7fb7cc8481384166e4b30fd84275f71
kesscode <kessyasantana@gmail.com>
2025-12-06 20:04:42 -0300

    Alterações (terceira leva):
    - Criei o read por id de produto

--- projeto/src/model/dao/impl/ProdutoDAOJDBC.java
+++ projeto/src/model/dao/impl/ProdutoDAOJDBC.java
@@ -2,12 +2,14 @@ package model.dao.impl;
 
 import db.Db;
 import exceptions.DbException;
+import exceptions.TipoInvalidoException;
 import model.dao.ProdutoDAO;
 import model.entities.Produto;
 import model.entities.ProdutoDuravel;
 import model.entities.ProdutoPerecivel;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,18 +59,92 @@ public class ProdutoDAOJDBC implements ProdutoDAO {
         }
     }
 
-    public Produto buscarPorId(int id){
-        return new ProdutoDuravel();
+    public Produto buscarPorId(int id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement("select * from produtos where id = ?");
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+            if (rs.next()) {
+                String nome = rs.getString("nome");
+                double p_compra = rs.getDouble("preco_compra");
+                double p_venda = rs.getDouble("preco_venda");
+                int qtd_estoque = rs.getInt("quantidade_estoque");
+                String tipoProd = rs.getString("tipo_produto");
+
+                try{
+                if (tipoProd.equals("PERECIVEL")) {
+                    java.sql.Date data = rs.getDate("data_validade");
+                    LocalDate dataValid;
+
+                    if (data != null)
+                        dataValid = data.toLocalDate();
+                    else
+                        dataValid = null;
+
+                    return new ProdutoPerecivel(id, nome, p_compra, p_venda, qtd_estoque, tipoProd, dataValid);
+                } else {
+                    String material = rs.getString("material");
+                    return new ProdutoDuravel(id, nome, p_compra, p_venda, qtd_estoque, tipoProd, material);
+                }
+                }catch (TipoInvalidoException e){
+                    throw new DbException("Dados do produto de ID " + id + " está mal-formatado: " + e.getMessage());
+                }
+            } return null;
+        } catch (SQLException e) {
+            throw new DbException("Erro ao buscar produto: " + e.getMessage());
+        } finally {
+            Db.closeResultSet(rs);
+            Db.closeStatement(st);
+        }
     }
 
     public List<Produto> buscarTodos(){
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement("");
+        }catch (SQLException e){
+            throw new DbException("Erro ao buscar fornecedor: " + e.getMessage());
+        }finally {
+            Db.closeStatement(st);
+            Db.closeResultSet(rs);
+        }
         return new ArrayList<>();
     }
 
-    public void atualizar(Produto p){}
+    public void atualizar(Produto p){
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement("");
+        }catch (SQLException e){
+            throw new DbException("Erro ao buscar fornecedor: " + e.getMessage());
+        }finally {
+            Db.closeStatement(st);
+            Db.closeResultSet(rs);
+        }
+    }
 
 
     //Delete (se não tiver relacionado a nenhuma transação)
-    public void deletarPorId(int id){}
+    public void deletarPorId(int id){
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement("");
+        }catch (SQLException e){
+            throw new DbException("Erro ao buscar fornecedor: " + e.getMessage());
+        }finally {
+            Db.closeStatement(st);
+            Db.closeResultSet(rs);
+        }
+    }
 
 }
