26bde19253e01f1cf2dc14ad5d329dd9f956a64e
kesscode <kessyasantana@gmail.com>
2025-12-07 16:20:09 -0300

    Alterações (setima leva incompleta):
    - Continuacao

--- projeto/src/model/dao/impl/TransacaoEstoqueDAOJDBC.java
+++ projeto/src/model/dao/impl/TransacaoEstoqueDAOJDBC.java
@@ -1,11 +1,12 @@
 package model.dao.impl;
 
+import db.Db;
 import exceptions.DbException;
 import model.dao.TransacaoEstoqueDAO;
 import model.entities.TransacaoEstoque;
 
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,7 +47,7 @@ public class TransacaoEstoqueDAOJDBC implements TransacaoEstoqueDAO {
 
             if("ENTRADA".equals(tr.getTipoMovimento())) {
                 st.setInt(6, tr.getIdFornecedor());
-            } else if("SAIDA".equals(tr.getTipoMovimento())) {
+            }else if("SAIDA".equals(tr.getTipoMovimento())) {
                 st.setNull(7, Types.INTEGER);
             }
 
@@ -76,17 +77,60 @@ public class TransacaoEstoqueDAOJDBC implements TransacaoEstoqueDAO {
     }
 
     private void atualizarEstoqueProduto(TransacaoEstoque tr, Connection conn, PreparedStatement st, ResultSet rs) throws SQLException{
+        //verificação do tipo do produto referenciado na transação atual (conferir se batem)
+        try {
+            st = conn.prepareStatement("select produtos.tipo_produto, produtos.data_validade " +
+                    "from produtos join transacoes_estoque " +
+                    "on produtos.id = transacoes_estoque.id_produto " +
+                    "where transacoes_estoque.id = ?");
 
-        st = conn.prepareStatement("select produtos.tipo_produto " +
-                                        "from produtos join transacoes_estoque " +
-                                        "on produtos.id = transacoes_estoque.id_produto " +
-                                        "where transacoes_estoque.id = ?");
+            st.setInt(1, tr.getId());
+            rs = st.executeQuery();
 
-        /*st = conn.prepareStatement("update produtos set quantidade_estoque = ?, data_validade = ? where id = ?");
+            String tipoProduto = rs.getString("tipo_produto");
+            LocalDate dataValidade = rs.getObject("data_validade", LocalDate.class);
 
-        st.setInt(1, tr.getQuantidade());
-        if()
-        st.setDate();*/
+            //se o produto referenciado for duravel e tem data de validade, erro!
+            if (("DURAVEL").equals(tipoProduto) && tr.getDataValidadeLote() != null) {
+                throw new DbException("Tipo do produto de ID " + tr.getIdProduto() + " não equivale ao informado");
+            }
+            //se o produto referenciado for perecivel, é uma entrada e não tem data de validade, erro!
+            if (("PERECIVEL").equals(tipoProduto) && "ENTRADA".equals(tr.getTipoMovimento()) && tr.getDataValidadeLote() != null) {
+                throw new DbException("Tipo do produto de ID " + tr.getIdProduto() + " não equivale ao informado");
+            }
 
+
+            //se passou, tá tudo certo. agora atualizamos o estoque/data
+            if (("PERECIVEL").equals(tipoProduto) && "ENTRADA".equals(tr.getTipoMovimento())) {
+                st = conn.prepareStatement("update produtos set quantidade_estoque = quantidade_estoque + ?, data_validade = ?, where id = ?");
+                st.setInt(1, tr.getQuantidade());
+
+
+                //lógica para salvar o lote mais próximo de vencer:
+                if (dataValidade == null)
+                    st.setDate(2, Date.valueOf(tr.getDataValidadeLote()));
+                else {
+                    if (dataValidade.isAfter(tr.getDataValidadeLote()))
+                        st.setDate(2, Date.valueOf(tr.getDataValidadeLote()));
+                    else
+                        st.setDate(2, Date.valueOf(dataValidade));
+                }
+
+                st.setInt(3, tr.getIdProduto());
+            } else if (("DURAVEL").equals(tipoProduto) && "ENTRADA".equals(tr.getTipoMovimento())) {
+                st = conn.prepareStatement("update produtos set quantidade_estoque = quantidade_estoque + ?, where id = ?");
+                st.setInt(1, tr.getQuantidade());
+                st.setInt(2, tr.getIdProduto());
+
+            } else if ("SAIDA".equals(tr.getTipoMovimento())) {
+                st = conn.prepareStatement("update produtos set quantidade_estoque = quantidade_estoque - ?, where id = ?");
+                st.setInt(1, tr.getQuantidade());
+                st.setInt(2, tr.getIdProduto());
+            }
+
+            st.executeUpdate();
+        } catch (DbException e){
+            System.out.println("Erro ao atualizar estoque: " + e.getMessage());
+        }
     }
 }
